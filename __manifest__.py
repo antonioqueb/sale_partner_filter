1cef48ed6c0f4490f2fd2ff9fde1f3487bd38057
Antonio Queb <antonioqueb@alphaqueb.online>
2025-02-04 12:16:25 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Sale Partner Filter",
-    "version": "17",
+    "version": '17.0.1.0.0',
     "summary": "Filtra productos en líneas de venta según company_registry del cliente.",
     "author": "Alphaqueb Consulting SAS",
     "website": "https://alphaqueb.com",
